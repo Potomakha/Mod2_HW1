1e6bc649b96aaac6d1a963fb6f5b36a4679d7116
Oleh Potomakha <potomakha@gmail.com>
2021-11-10 15:23:33 +0200

    Add class Result

---
+++ Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_HW1
+{
+    /// <summary>
+    /// Specify structure for output log results.
+    /// </summary>
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string Text { get; set; }
+    }
+}
