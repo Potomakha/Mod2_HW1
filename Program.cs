a79f40c6c627f96b91d2ce268f45715644607b21
Oleh Potomakha <potomakha@gmail.com>
2021-11-10 14:33:37 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod2_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
