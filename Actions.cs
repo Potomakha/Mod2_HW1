75b3ab478c5e750ff49ccef60991ad8334d978c0
Oleh Potomakha <potomakha@gmail.com>
2021-11-10 15:33:17 +0200

    add Actions

---
+++ Actions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_HW1
+{
+    /// <summary>
+    /// imitation of call logger.
+    /// </summary>
+    public class Actions
+    {
+        public Result InfoMethod()
+        {
+            var result = new Result { Status = true };
+            return result;
+        }
+
+        public Result WarningMethod()
+        {
+            var result = new Result { Status = true };
+            return result;
+        }
+
+        public Result ErrorMethod()
+        {
+            var result = new Result { Status = false, Text = "I broke a logic" };
+            return result;
+        }
+    }
+}
