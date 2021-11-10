3fb86c5dc2deb78e70205876769ee8c43fdaeb45
Oleh Potomakha <potomakha@gmail.com>
2021-11-10 15:14:28 +0200

    Add Starter

---
+++ Starter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_HW1
+{
+    /// <summary>
+    /// Starter calss.
+    /// </summary>
+    public class Starter
+    {
+        public void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+            }
+        }
+    }
+}
